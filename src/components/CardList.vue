beb4f714b3c162ee2111027c6f7e51cba1c053e4
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-12 17:07:02 +0200

    fixed card component

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -2,32 +2,66 @@
     <div class="container">
         <div class="row">
             <div class="col-3" v-for="(coktail, index) in coktail_list" :key="index" >
-                <CardComponent :coktail="coktail"/>
+                <div class="card" >
+            <img :src="coktail.strDrinkThumb" class="card-img-top" :alt="coktail.strDrink">
+            <div class="card-body">
+                <h5 class="card-title">{{ coktail.strDrink }}</h5>
+            </div>
+        </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import axios from 'axios';
-import CardComponent from './CardComponent.vue'
     export default {
         name: 'CardList',
-        component: {
-            CardComponent,
-        },
+       
         data() {
             return {
-                api_path:'www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic',
-                coktail_list: [],
+                coktail_list:[
+                    {
+                    strDrink: "110 in the shade",
+                    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/xxyywq1454511117.jpg",
+                    idDrink: "15423"
+                    },
+                    {
+                    strDrink: "151 Florida Bushwacker",
+                    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/rvwrvv1468877323.jpg",
+                    idDrink: "14588"
+                    },
+                    {
+                    strDrink: "110 in the shade",
+                    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/xxyywq1454511117.jpg",
+                    idDrink: "15423"
+                    },
+                    {
+                    strDrink: "151 Florida Bushwacker",
+                    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/rvwrvv1468877323.jpg",
+                    idDrink: "14588"
+                    }, 
+                    {
+                    strDrink: "110 in the shade",
+                    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/xxyywq1454511117.jpg",
+                    idDrink: "15423"
+                    },
+                    {
+                    strDrink: "151 Florida Bushwacker",
+                    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/rvwrvv1468877323.jpg",
+                    idDrink: "14588"
+                    },
+                    {
+                    strDrink: "151 Florida Bushwacker",
+                    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/rvwrvv1468877323.jpg",
+                    idDrink: "14588"
+                    },{
+                    strDrink: "151 Florida Bushwacker",
+                    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/rvwrvv1468877323.jpg",
+                    idDrink: "14588"
+                    },
+                ],
             }
         },
-        mounted() {
-            axios.get(this.api_path)
-                .then((response) => {
-                    this.coktail_list=response.data.drinks
-            })
-        }
     }
 </script>
 
