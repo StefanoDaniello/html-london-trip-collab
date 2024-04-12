14be4e5e5d690446ece64bc6958c17f5bc4aa72f
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-12 16:09:50 +0200

    add components in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <h1 class="text-center">Ciao</h1>
-  <i class="fa fa-solid fa-home"></i>
+  <HeaderComponent/>
+  <MainTop/>
+  <MainComponent/>
+  <FooterComponent/>
+  <FooterBottom/>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainTop  from './components/MainTop.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import FooterBottom from './components/FooterBottom.vue';
   export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        HeaderComponent,
+        MainTop,
+        MainComponent,
+        FooterComponent,
+        FooterBottom
+    },
   }
 </script>
 
