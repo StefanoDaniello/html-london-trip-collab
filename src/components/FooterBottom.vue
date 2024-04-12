655fa5dad24e6d8e88eed40128940f07bdb119a1
samuelescavello <scavellosamuele23@gmail.com>
2024-04-12 15:44:40 +0200

    footer-bottom

---
+++ src/components/FooterBottom.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3 class ="text-center text-uppercase">questo é il footer bottom</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterBottom'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
